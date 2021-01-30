599b86e6d7747e6388090925e7634f04a5f181ba
Chronically Exhausted <london.victoria.s@gmail.com>
2021-01-30 16:58:03 +0000

    commented old commands

--- main.py
+++ main.py
@@ -27,45 +27,45 @@ async def on_member_join(member):
 async def on_member_remove(member):
   print(f'{member} died, we might miss them')
 
-@client.command()
-async def boop(ctx):
-  await ctx.send('Boo!')
+# @client.command()
+# async def boop(ctx):
+#   await ctx.send('Boo!')
 
-@client.command()
-async def ping(ctx):
-  await ctx.send(f'Pong! {client.latency * 1000}ms')
+# @client.command()
+# async def ping(ctx):
+#   await ctx.send(f'Pong! {client.latency * 1000}ms')
 
-@client.command(aliases=['8ball'])
-async def _8ball(ctx,*,question):
-  response = [
-    'likely','maybe','unlikely','um sure', 'poopoo', 'Yes.', 'check Google idiot i\'ve got better things to do'
-  ]
-  await ctx.send(f'Question: {question}\nAnswer: {random.choice(response)}')
+# @client.command(aliases=['8ball'])
+# async def _8ball(ctx,*,question):
+#   response = [
+#     'likely','maybe','unlikely','um sure', 'poopoo', 'Yes.', 'check Google idiot i\'ve got better things to do'
+#   ]
+#   await ctx.send(f'Question: {question}\nAnswer: {random.choice(response)}')
 
-@client.command()
-async def clear(ctx,amount=6):
-  await ctx.channel.purge(limit=amount)
+# @client.command()
+# async def clear(ctx,amount=6):
+#   await ctx.channel.purge(limit=amount)
 
-@client.command()
-async def kick(ctx, member : discord.Member, *,reason='nope'):
-  await member.kick(reason=reason)
+# @client.command()
+# async def kick(ctx, member : discord.Member, *,reason='nope'):
+#   await member.kick(reason=reason)
 
-@client.command()
-async def ban(ctx, member : discord.Member, *,reason='no'):
-  await member.ban(reason=reason)
+# @client.command()
+# async def ban(ctx, member : discord.Member, *,reason='no'):
+#   await member.ban(reason=reason)
 
-@client.command()
-async def unban(ctx, *, member):
-  bannedUsers= await ctx.guild.bans()
-  memberName, memberDiscriminator = member.split('#')
+# @client.command()
+# async def unban(ctx, *, member):
+#   bannedUsers= await ctx.guild.bans()
+#   memberName, memberDiscriminator = member.split('#')
 
-  for banEntry in bannedUsers:
-    user = banEntry.user
+#   for banEntry in bannedUsers:
+#     user = banEntry.user
 
-    if(user.name,user.discriminator) == (memberName,memberDiscriminator):
-      await ctx.guild.unban(user)
-      await ctx.send('unbanned {user}'.format(user=memberName))
-      return
+#     if(user.name,user.discriminator) == (memberName,memberDiscriminator):
+#       await ctx.guild.unban(user)
+#       await ctx.send('unbanned {user}'.format(user=memberName))
+#       return
 
 # @client.command
 # async def play(ctx,*,song):
