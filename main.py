7c7724352bfb7534948978c5774e60377d2d2189
Xichao <thexichao@gmail.com>
2021-01-31 16:01:49 +0000

    level system working

--- main.py
+++ main.py
@@ -15,59 +15,68 @@ async def on_ready():
   print('Bot is ready')
 
 
-
 @client.event
 async def on_member_join(member):
-  with open('users.json','r') as f:
-    users = json.load(f)
+    with open('users.json', 'r') as f:
+        users = json.load(f)
 
-    await  updateData(users, member)
+    await updateData(users, member)
 
-
-    
-  with open('users.json','w') as f:
-    json.dump(users,f)
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
 
 
 @client.event
 async def on_message(message):
-  with open('users.json','r') as f:
-    users = json.load(f)
+    if message.author.bot == False:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
 
-  await updateData(users, message.author)
-  await addExperience(users,message.author, 5)
-  await levelUp(users, message.author, message.channel)
+        await updateData(users, message.author)
+        await addExperience(users, message.author, 5)
+        await levelUp(users, message.author, message)
+
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+
+    await client.process_commands(message)
 
 
-  with open('users.json','w') as f:
-    json.dump(users,f)
+async def updateData(users, user):
+    if not f'{user.id}' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['experience'] = 0
+        users[f'{user.id}']['level'] = 1
 
-async def updateData(users,user):
-  if not user.id in users:
-    users[user.id] = {}
-    users[user.id]['experience'] = 0
-    users[user.id]['level'] = 1
 
 async def addExperience(users, user, exp):
-  users[user.id]['experience'] += exp
-
-async def levelUp(users, user, channel):
-  experience = users[user.id]['experience']
-  lvlStart = users[user.id]['level']
-  lvlEnd = int(experience ** (1/4))
-
-  if lvlStart < lvlEnd:
-    await client.send_message(channel,'Congrats {} has leveled up to level {}'.format(user.mention,lvlEnd))
-    users[user.id]['level'] = lvlEnd
-
-  
-
-@client.event
-async def on_member_remove(member):
-  print(f'{member} died, we might miss them')
+    users[f'{user.id}']['experience'] += exp
 
 
+async def levelUp(users, user, message):
+    with open('levels.json', 'r') as g:
+        levels = json.load(g)
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1 / 4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
 
+@client.command()
+async def level(ctx, member: discord.Member = None):
+    if not member:
+        id = ctx.message.author.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'You are at level {lvl}!')
+    else:
+        id = member.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'{member} is at level {lvl}!')
 
 
 @client.command()
